276c3bc9e5142e9191b6088eceb95c56cabb8c8c
Haruka Tsuchiya <solap5hyk237@gmail.com>
2023-07-08 18:51:53 +0900

    現在の日時を出力

--- src/main/java/com/haruka/helloworld/HelloWorldController.java
+++ src/main/java/com/haruka/helloworld/HelloWorldController.java
@@ -3,12 +3,15 @@ package com.haruka.helloworld;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @RestController
 public class HelloWorldController {
     @GetMapping("/hello")
-    public String hello() {
-        String hello = "Hello World";
-        return hello;
-    }
+    public Date date() {
 
+        Date date = new Date(LocalDateTime.now());
+        return date;
+
+    }
 }
