0e8292b9b7a6ae3f4b1b456cbb23b3f010a4d35f
Haruka Tsuchiya <solap5hyk237@gmail.com>
2023-07-08 14:41:16 +0900

    Hello Worldを出力

---
+++ src/main/java/com/haruka/helloworld/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.haruka.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/hello")
+    public String hello() {
+        String hello = "Hello World";
+        return hello;
+    }
+
+}
